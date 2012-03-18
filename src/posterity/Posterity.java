871ffadb71e38c414400cb891fb79e0fa803a2b6
Galyna Zholtkevych <galka9291@gmail.com>
2012-03-18 19:36:25 +0200

    Push Posterity & PosterityBehaviour

--- src/posterity/Posterity.java
+++ src/posterity/Posterity.java
@@ -1,11 +1,35 @@
-package posterity;
-
+package posterity;	
+import genotype.Genotype;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 
-public class Posterity extends Agent {
+import java.io.Reader;
+import java.util.HashSet;
 
-	private static final long serialVersionUID = 1L;
-	
-	// TODO
+import settings.PosterityParentsPair;
+import settings.PosterityResultPair;
+import settings.Vocabulary;
 
-}
+	public class Posterity extends Agent implements Vocabulary{
+		private AID id;
+		
+		private static final long serialVersionUID = 1L;
+				
+		@SuppressWarnings("serial")
+		protected void setup(){
+			//i must to obtain parents. When Female create me, she must send to me Male and Female Genotype
+			
+			String nickname = "Posterity";
+	    	id = new AID(nickname,AID.ISLOCALNAME);  
+	    	Object[] args = getArguments();//в аргументах должны быть 2 генотипа
+	  		if (args.length < 2)
+	  			return;
+	  		Genotype male =  (Genotype) args[0];
+	  		Genotype female = (Genotype) args[1];
+	  		PosterityParentsPair parentsPair = new PosterityParentsPair(female,male);
+			this.addBehaviour(new PosterityBehaviour(parentsPair));
+			//получим пару родителей и образуем результирующую пару и передадим ее зоне
+			
+		}
+	}
