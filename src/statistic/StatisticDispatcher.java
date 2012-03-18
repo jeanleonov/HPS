642446b9b9676d351fad0dc9e62e9a9d114fdf40
anplug <anplug@gmail.com>
2012-03-18 18:13:21 +0200

    some++

--- src/statistic/StatisticDispatcher.java
+++ src/statistic/StatisticDispatcher.java
@@ -15,7 +15,7 @@ import jxl.write.WritableWorkbook;
 import jxl.write.WriteException;
 import jxl.write.biff.RowsExceededException;
 
-public class StatisticDispatcher extends Agent{			// WHY IT IS NOT AGENT???
+public class StatisticDispatcher extends Agent{
 
 	static int currentRow = 0;
 
