f16ec29b6a8b888812ed61f5bd6f3c93abca958c
Anton Leonov <jeanleonov@gmail.com>
2012-03-22 00:19:38 +0200

    FFFFFFFFF genotype fixing

--- src/genotype/Genotype.java
+++ src/genotype/Genotype.java
@@ -57,13 +57,13 @@ public class Genotype implements Serializable {
 			mustContinue=false;
 			if (genomes.length != genotypes.get(i).genomes.length)	continue;
 			for (int j=0; j<clonalities.length; j++)
-				if (clonalities[j] != genotypes.get(j).clonalities[j]){
+				if (clonalities[j] != genotypes.get(i).clonalities[j]){
 					mustContinue = true;
 					break;
 				}
 			if (mustContinue)	continue;
 			for (int j=0; j<genomes.length; j++)
-				if (!genomes[i].equals(genotypes.get(j).genomes[i])){
+				if (!genomes[j].equals(genotypes.get(i).genomes[j])){
 					mustContinue = true;
 					break;
 				}
