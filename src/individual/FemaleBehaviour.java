150354bcb0c6161ab82802f9119e7bec2073cc07
Galyna Zholtkevych <galka9291@gmail.com>
2012-05-05 15:07:36 +0300

    Correction of mistakes

--- src/individual/FemaleBehaviour.java
+++ src/individual/FemaleBehaviour.java
@@ -20,34 +20,39 @@ import settings.PosterityResultPair;
 public class FemaleBehaviour extends IndividualBehaviour {
 
 	private static final long serialVersionUID = 1L;
-	PosterityParentsPair parents;
-	ArrayList<PosterityResultPair> resultsInterbreeding=null;
-	float maleFertility; 
-	float femaleFertility;
-	Individual my_mother;
-	public FemaleBehaviour(Genotype male, Float maleFertility) {
+
+	
+	/*public FemaleBehaviour(Genotype male, Float maleFertility) {
 		// TODO Auto-generated constructor stub
 		my_mother = (Individual)myAgent;
 		Genotype female = (Genotype) my_mother.getGenotype();
 		this.parents = new PosterityParentsPair(male, female);
 		this.maleFertility = (float)maleFertility;
 		this.femaleFertility = my_mother.getSetting(settings.Vocabulary.Param.Fertility);
-	}
+	}*/
 
+	public void onStart(){
+		
+	}
+	
+	
 	public void action() {
-		// TODO Auto-generated method stub		
-		ArrayList<PosterityResultPair> resultsInterbreeding = getResultPairs();				
+		// TODO Auto-generated method stub
+		Individual my_mother = (Individual)myAgent;
+		Genotype male=null, female = (Genotype) my_mother.getGenotype();
+		//male will be known after reproduct process;
+		float maleFertility=1, femaleFertility = my_mother.getSetting(settings.Vocabulary.Param.Fertility);
+		//maleFertility will be known after reproduction's process;
 		int posteritySize = (int)maleFertility * (int)femaleFertility;
-		/*IMPORTANT!!!
-		 * it is collection of distribution 
-		(genotype, probability_of_appearance_in_this_posterity*viability
-		 */
+		PosterityParentsPair parents = new PosterityParentsPair(male, female);
+		ArrayList<PosterityResultPair> resultsInterbreeding = getResultPairs(parents);
+		
 		for(int i=0; i<resultsInterbreeding.size();i++){
 			resultsInterbreeding.get(i).setProbability(resultsInterbreeding.get(i).getProbability()*posteritySize);
 		}	
 			
 		try {
-			sendSurvivedBerries();
+			sendSurvivedBerries(resultsInterbreeding);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -55,15 +60,17 @@ public class FemaleBehaviour extends IndividualBehaviour {
 		
 	}  
 	
-	public void sendSurvivedBerries() throws IOException{//send to Zone survived berries
+	public void sendSurvivedBerries(ArrayList<PosterityResultPair> resultsInterbreeding) throws IOException{//send to Zone survived berries
+		Individual my_mother = (Individual)myAgent;
 		ACLMessage survivedBerries = new ACLMessage(ACLMessage.INFORM);
 		survivedBerries.addReceiver(my_mother.getAID_Zone());
+		survivedBerries.setLanguage("Survived berries");
 		survivedBerries.setContentObject(resultsInterbreeding);
 		my_mother.send(survivedBerries);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public ArrayList<PosterityResultPair> getResultPairs(){	
+	public ArrayList<PosterityResultPair> getResultPairs(PosterityParentsPair parents){	
 		DFAgentDescription template = new DFAgentDescription();
   		ServiceDescription templateSd = new ServiceDescription();
   		templateSd.setType("Settings");
@@ -72,7 +79,14 @@ public class FemaleBehaviour extends IndividualBehaviour {
   		SearchConstraints sc = new SearchConstraints();
   		// We want to receive 1 result
   		sc.setMaxResults(new Long(1));
+  		
   		ArrayList<PosterityResultPair> resultsInterbreedingArray=null;
+
+		/*IMPORTANT!!!
+		 * it is collection of distribution 
+		(genotype, probability_of_appearance_in_this_posterity*viability
+		 */
+		
   		try {
   			DFAgentDescription[] results = null;
   			while(results == null || results.length < 1) {
