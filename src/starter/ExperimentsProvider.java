82d4f546efc4ae1a469c8fb6cdf05d00bafe0814
Anton <jeanleonov@gmail.com>
2012-07-21 06:13:11 +0300

    calculation of executing time was added

--- src/starter/ExperimentsProvider.java
+++ src/starter/ExperimentsProvider.java
@@ -13,24 +13,46 @@ public class ExperimentsProvider extends Behaviour implements Messaging {
 	private static final long serialVersionUID = 1L;
 	
 	private SystemStarter starter;
+	long timeOfStart;
+	boolean done;
+	int numberOfRuningExperiments;
 	
 	@Override
 	public void onStart() {
 		super.onStart();
 		starter = (SystemStarter)myAgent;
+		numberOfRuningExperiments = 0;
 		startExperiments();
+		done = false;
+		timeOfStart = System.currentTimeMillis();
 	}
 
 	@Override
 	public void action() {
 		ACLMessage response = starter.blockingReceive(MessageTemplate.MatchLanguage(I_FINISHED));
+		numberOfRuningExperiments--;
 		int nodeNumber = Integer.parseInt(response.getContent());
-		sendToContainerNewExperiment(nodeNumber);
+		if (starter.remainingExperiments > 0)
+			sendToContainerNewExperiment(nodeNumber);
+		else
+			done = true;
 	}
 
 	@Override
 	public boolean done() {
-		return starter.remainingExperiments <= 0;
+		return done;
+	}
+	
+	@Override
+	public int onEnd() {
+		long executingTime = System.currentTimeMillis()-timeOfStart,
+			 hour = executingTime/1000/60/60,
+			 min = executingTime/1000/60 - hour*60,
+			 sec = executingTime/1000 - min*60 - hour*3600,
+			 msec = executingTime - sec*1000 - min*60000 - hour*3600000; 
+		System.out.printf("--------------------------------\n"+
+						  "Executing time:	[%2s:%2s:%2s.%3s]",hour,min,sec,msec);
+		return super.onEnd();
 	}
 	
 	private void startExperiments(){
@@ -56,6 +78,7 @@ public class ExperimentsProvider extends Behaviour implements Messaging {
 			agent.start();
 			starter.remainingExperiments--;
 			starter.curExperiment++;
+			numberOfRuningExperiments++;
 		} catch (StaleProxyException e) {
 			e.printStackTrace();
 		}
